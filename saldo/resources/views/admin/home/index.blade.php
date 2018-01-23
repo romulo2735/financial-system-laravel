fea81545e40980782935002193892f5442fe2908
Romulo Sousa <romulosousa27@hotmail.com>
2018-01-23 00:17:28 -0300

    views

---
+++ saldo/resources/views/admin/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Home Painel')
+
+@section('content_header')
+    <h1>Painel de Controle</h1>
+@stop
+
+@section('content')
+    <p>You are logged in!</p>
+@stop
