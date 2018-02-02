104a47fce01e8af4a95c65cc13e4fe7d9a68c0b8
Romulo Sousa <romulosousa27@hotmail.com>
2018-02-01 23:04:15 -0300

    recarrega

--- saldo/app/Models/Balance.php
+++ saldo/app/Models/Balance.php
@@ -9,8 +9,21 @@ class Balance extends Model{
     /* Removido da migration, para não criar as colunas create_at e update_at */
     public $timestamps = false;
 
-    public function deposit($value){
+    public function deposit(float $value) : Array{
         
-        dd($value);
+        $this->amount += number_format($value, 2, '.', '') ;
+        $deposit = $this->save();
+
+        if ($deposit){
+            return[
+                'sucess'    =>  true,
+                'messege'   =>  'Sucesso ao recerregar'
+            ];
+            return[
+                'sucess'    =>  false,
+                'messege'   =>  'Falha ao recerregar'
+            ];
+        }
+
     }
 }
