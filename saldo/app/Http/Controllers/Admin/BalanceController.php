d1d895f66156a357ed50b54989b09043d2f2450c
Romulo Sousa <romulosousa27@hotmail.com>
2018-02-06 22:06:40 -0300

    tratamento de mensagem de erros

--- saldo/app/Http/Controllers/Admin/BalanceController.php
+++ saldo/app/Http/Controllers/Admin/BalanceController.php
@@ -25,7 +25,11 @@ class BalanceController extends Controller
     public function depositStore(MoneyValidationFormRequest $request){
 
         $balance = auth()->user()->balance()->firstOrCreate([]);
-        dd($balance->deposit($request->value));
-        
+        $response = $balance->deposit($request->value);
+
+        if ($response['success'])
+            return redirect()->route('admin.balance')->with('success', $response['message']);
+
+            return redirect()->back()->with('error', $response['message']);
     }
 }
