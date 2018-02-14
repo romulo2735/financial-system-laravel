6b7ca373c0a6c20c061997f2b0cbc5f6fcc5d1b4
romulo alves de sousa <romulosousa27@hotmail.com>
2018-02-14 15:04:18 -0300

    rotas de tranferecia

--- saldo/routes/web.php
+++ saldo/routes/web.php
@@ -4,10 +4,15 @@
 $this->group(['middleware' => 'auth' , 'namespace' => 'Admin', 'prefix' => 'admin'] ,  function(){
     $this->get('/' , 'AdminController@index')->name('admin.home');
     $this->get('balance' , 'BalanceController@index')->name('admin.balance');
+
     $this->get('deposit' , 'BalanceController@deposit')->name('balance.deposit');
     $this->post('deposit' , 'BalanceController@depositStore')->name('deposit.store');
+
     $this->get('saque' , 'BalanceController@saque')->name('balance.saque');
     $this->post('saque' , 'BalanceController@saqueStore')->name('saque.store');
+
+    $this->get('transfer' , 'BalanceController@transfer')->name('balance.transfer');
+    $this->post('transfer' , 'BalanceController@transferStore')->name('transfer.store');
 });
 
 
