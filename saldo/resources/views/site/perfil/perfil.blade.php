2c86cd8bbd3916641d6c0dfcbb4a762ec38e6dee
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-03 15:46:38 -0300

    perfil

---
+++ saldo/resources/views/site/perfil/perfil.blade.php
@@ -0,0 +1,37 @@
+@extends('site.layouts.layout')
+@section('title', 'Meu Perfil')
+
+@section('content')
+    <h1>Meu Perfil</h1>
+    <form action="{{route('perfil-atualizar')}}" method="post">
+        {!! csrf_field() !!}
+        <div class="form-group">
+            <label for="name">Nome: </label>
+            <input class="form-control" value="{{ auth()->user()->name  }}" type="text" name="name" placeholder="Nome">
+        </div>
+        <div class="form-group">
+            <label for="email">E-Mail: </label>
+            <input class="form-control" value="{{ auth()->user()->email  }}" type="text" name="email" placeholder="E-mail">
+        </div>
+        <div class="form-group">
+            <label for="password">Senha: </label>
+            <input class="form-control" value="{{auth()->user()->password}}" type="password" name="password" placeholder="Senha">
+        </div>
+        <div class="form-group">
+            <label for="image">Imagem: </label>
+            <input class="form-control" type="file" name="image">
+        </div>
+        <div class="form-group">
+            <button  type="submit" class="btn btn-info">Atualizar Perfil</button>
+        </div>
+    </form>
+
+
+
+
+
+
+
+
+
+@endsection
