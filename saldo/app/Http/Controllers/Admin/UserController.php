8a97fdcaeab14f85d49472fd3d49f4ef50103ffd
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-03 18:04:15 -0300

    editando perfil de usuarios

--- saldo/app/Http/Controllers/Admin/UserController.php
+++ saldo/app/Http/Controllers/Admin/UserController.php
@@ -13,6 +13,22 @@ class UserController extends Controller
     }
 
     public function PerfilAtualizar(Request $request){
-        
+        $dados = $request->all();
+
+        //verificando senha, caso o User tenha modifcado.
+        if ($dados['password'] != null){
+            $dados['password'] = bcrypt($dados['password']);
+        }
+        else{
+            unset($dados['password']);
+        }
+
+        //atualizar apenas usuario logado.
+        $att = auth()->user()->update();
+
+        if ($att){
+            return redirect()->route('perfil')->with('success', 'Sucesso ao Atualizar o Perfil');
+        }
+        return redirect()->back()->with('error', 'Falha ao Atualizar o Perfil');
     }
 }
