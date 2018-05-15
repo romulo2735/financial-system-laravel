27c494ba325781b1795ca915dfbf42ff7c076cb8
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-15 16:40:56 -0300

    request profile update

---
+++ saldo/app/Http/Requests/UpdateProfileFormRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $idUser = auth()->user()->id;
+        return [
+            'name'      =>  'required|string|max:100',
+            'email'     =>  "required|string|email|max:100|unique|users,email,{$idUser},id",
+            'password'  =>  'required|numeric|min:6',
+            'image'     =>  'image'
+        ];
+    }
+}
